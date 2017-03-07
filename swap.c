0131304f8caa138d4ca2aa8b68139f589537669a
Omar Flórez <omarmfs98@protonmail.com>
2017-03-06 19:40:09 -0500

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 34;
+    int y = 50;
+    
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
